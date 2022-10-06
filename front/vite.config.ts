b75f80525bde580bd0a2cca8577918aed3749eae
Jhon Jairo Sanchez Benavides <j.jairosanchez.b@gmail.com>
2022-10-06 00:32:59 +0000

    github depoyment changes

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/photograpy/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
